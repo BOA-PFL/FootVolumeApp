e31add450f7dd7ee1a421f9e2dfe23baf71ece9c
Bethany Kilpatrick <85694373+Bethany-Kilpatrick@users.noreply.github.com>
2022-09-22 13:12:48 -0600

    Update nameEncoder.R

--- nameEncoder.R
+++ nameEncoder.R
@@ -2,7 +2,8 @@ rm(list=ls())
 
 library(tidyverse)
 
-dat <- read.csv('C:/Users/daniel.feeney/Boa Technology Inc/PFL Team - General/BigData2021/FootScan Data/MasterSubjectSizes_Male.csv')
+dat <- read.csv('C:/Users/bethany.kilpatrick/Boa Technology Inc/PFL - General/BigData/FootScan Data/MasterSubjectSizes_Female.csv') 
+
 dat$TypeofTester <- as.factor(dat$TypeofTester)
 
 num_list <- sample(1:1000, 500)
@@ -16,7 +17,26 @@ for (row in 1:dim(dat)[1]){
     else {
       intOrExt[row] <- dat$Subject[row]
     }
-}
+} 
+ 
 
-dat$codedNames <- intOrExt
+# 
+
+
+
+# 
+EU_Size <- c(dat$ShoeSize +33) #Male EU size conversion
+ 
+dat$EU_Size <- c(dat$ShoeSize +33) 
+
+EU_Size <- c(dat$ShoeSize +31)  # Female EU size conversion
+dat$EU_Size <- c(dat$ShoeSize +31)
+
+
+
+dat$codedNames <- intOrExt 
+ 
+
+
+write.csv(dat,"C:\\Users\\bethany.kilpatrick\\OneDrive - Boa Technology Inc\\Documents\\MasterSubjectSizes_Female_CodedKey.csv" , all(T) ) 
 
